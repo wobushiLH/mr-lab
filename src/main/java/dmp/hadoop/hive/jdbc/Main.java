fe11b76254aac185ef6b6591576a5fdee38228de
he.li <lihe.career@hotmail.com>
2015-12-29 15:03:46 +0800

    added group by sql statement in hive jdbc pkg

--- src/main/java/dmp/hadoop/hive/jdbc/Main.java
+++ src/main/java/dmp/hadoop/hive/jdbc/Main.java
@@ -1,8 +1,6 @@
 package dmp.hadoop.hive.jdbc;
 
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
+
 
 public class Main {
 
@@ -12,8 +10,12 @@ public class Main {
 		// TODO Auto-generated method stub
 
 //		String hqlCommand = "select * from dsp_bid_all_partitioned2 where dt='2015-12-01' limit 5";
-		String hqlCommand = "select get_json_object(dsp_cm_all_partitioned.json,\"$.source\"), get_json_object(dsp_cm_all_partitioned.json,\"$.hdtUID\"), get_json_object(dsp_cm_all_partitioned.json,\"$.ptUID\"), parse_url(get_json_object(dsp_bid_all_partitioned2.json,\"$.page\"),\"HOST\"), get_json_object(dsp_bid_all_partitioned2.json,\"$.nurl\") from dsp_cm_all_partitioned join dsp_bid_all_partitioned2 on get_json_object(dsp_cm_all_partitioned.json,\"$.hdtUID\") = get_json_object(dsp_bid_all_partitioned2.json,\"$.hdtcid\") where dsp_bid_all_partitioned2.dt = '2015-12-11' and dsp_cm_all_partitioned.dt = '2015-11-13' and get_json_object(dsp_cm_all_partitioned.json,\"$.source\")='miaozhen' limit 30";
+//		String hqlCommand = "select get_json_object(dsp_cm_all_partitioned.json,\"$.source\"), get_json_object(dsp_cm_all_partitioned.json,\"$.hdtUID\"), get_json_object(dsp_cm_all_partitioned.json,\"$.ptUID\"), parse_url(get_json_object(dsp_bid_all_partitioned2.json,\"$.page\"),\"HOST\"), get_json_object(dsp_bid_all_partitioned2.json,\"$.nurl\") from dsp_cm_all_partitioned join dsp_bid_all_partitioned2 on get_json_object(dsp_cm_all_partitioned.json,\"$.hdtUID\") = get_json_object(dsp_bid_all_partitioned2.json,\"$.hdtcid\") where dsp_bid_all_partitioned2.dt = '2015-12-11' and dsp_cm_all_partitioned.dt = '2015-11-13' and get_json_object(dsp_cm_all_partitioned.json,\"$.source\")='miaozhen' limit 30";
 
+		
+		String hqlCommand = "select parse_url(get_json_object(dsp_bid_all_partitioned2.json,\"$.page\"),\"HOST\"), count(*) from dsp_cm_all_partitioned join dsp_bid_all_partitioned2 on get_json_object(dsp_cm_all_partitioned.json,\"$.hdtUID\") = get_json_object(dsp_bid_all_partitioned2.json,\"$.hdtcid\") where dsp_bid_all_partitioned2.dt = '2015-12-11' and dsp_cm_all_partitioned.dt = '2015-11-13' and get_json_object(dsp_cm_all_partitioned.json,\"$.source\")='miaozhen' group by parse_url(get_json_object(dsp_bid_all_partitioned2.json,\"$.page\"),\"HOST\") limit 100";
+
+		
 		HiveJdbcClient hiveClient = new HiveJdbcClient();
 
 		/**
